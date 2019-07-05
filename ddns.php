a51d3424a4cb7b6472fcd366c98d2bb3aa56f069
xuecanlong <xuecanlong@qq.com>
2019-07-05 20:56:00 +0800

    test

--- ddns.php
+++ ddns.php
@@ -95,6 +95,22 @@ function get_client_ip(){
         $ip = $_SERVER['REMOTE_ADDR'];  
     else  
         $ip = "unknown";
+
+
+    // if (@$_SERVER["HTTP_X_FORWARDED_FOR"]) 
+    //     $ip = $_SERVER["HTTP_X_FORWARDED_FOR"]; 
+    // else if (@$_SERVER["HTTP_CLIENT_IP"]) 
+    //     $ip = $_SERVER["HTTP_CLIENT_IP"]; 
+    // else if (@$_SERVER["REMOTE_ADDR"]) 
+    //     $ip = $_SERVER["REMOTE_ADDR"]; 
+    // else if (@getenv("HTTP_X_FORWARDED_FOR")) 
+    //     $ip = getenv("HTTP_X_FORWARDED_FOR"); 
+    // else if (@getenv("HTTP_CLIENT_IP")) 
+    //     $ip = getenv("HTTP_CLIENT_IP"); 
+    // else if (@getenv("REMOTE_ADDR")) 
+    //     $ip = getenv("REMOTE_ADDR"); 
+    // else  
+    //     $ip = "unknown";
     return $ip;  
 }
 
